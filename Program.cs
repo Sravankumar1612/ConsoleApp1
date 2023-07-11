cc80812e9d473b2f1cc32564f52329bfbc945ef1
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-11 09:36:22 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            string firstname;
+            string lastname;
+            Console.WriteLine("Enter name");
+            firstname = Console.ReadLine();
+            Console.WriteLine("enter l name");
+            lastname = Console.ReadLine();
+            Console.WriteLine("welcome" + firstname + " " + lastname);
+
+            Console.ReadKey();
+        }
+    }
+}
