982257f55cc3931b9df2fd688f74aa1358d160fc
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-11 10:25:06 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +11,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            
-           
-            string firstname;
-            string lastname;
-            Console.WriteLine("Enter name");
-            firstname = Console.ReadLine();
-            Console.WriteLine("enter l name");
-            lastname = Console.ReadLine();
-            Console.WriteLine("welcome" + firstname + " " + lastname);
 
+
+            int empId;
+            string empName;
+            string empGender;
+            double empSal;
+            DateTime doj;
+
+
+            Console.WriteLine("Employee ID");
+            empId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Employee Name");
+            empName = Console.ReadLine();
+            Console.WriteLine("Employee Gender");
+            empGender = Console.ReadLine();
+            Console.WriteLine("Employee Salary");
+            empSal = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            doj = DateTime.Parse(Console.ReadLine());
+            
+            Console.WriteLine("ID\t" + "Employee Name\t" + "Employee Gender\t" +
+                "Employee Salary\t" + "Date of Joining");
+            Console.WriteLine(empId + "\t" + empName + "\t" + empGender + "\t" + empSal + "\t" + doj);
+            
+            if (empSal > 90000)
+            {
+                Console.WriteLine("You have to pay 30%tax");
+            }
+            else
+            {
+                Console.WriteLine("You have to pay 15% tax");
+            }
             Console.ReadKey();
         }
     }
